b7a9824b56c751202b5dc6f1ebc28b29b2053790
Iklil <iklilblender@gmail.com>
2024-12-01 14:28:15 +0700

    feat add: Listing resource filament

---
+++ RentHouse-Backend/app/Filament/Resources/ListingResource.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ListingResource\Pages;
+use App\Filament\Resources\ListingResource\RelationManagers;
+use App\Models\Listing;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ListingResource extends Resource
+{
+    protected static ?string $model = Listing::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-home-modern';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('address')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('sqft')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Forms\Components\TextInput::make('wifi_speed')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Forms\Components\TextInput::make('max_person')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Forms\Components\TextInput::make('price_per_day')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Forms\Components\Textarea::make('attachments')
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('full_support_available')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Forms\Components\TextInput::make('gym_area_available')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Forms\Components\TextInput::make('mini_cafe_available')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Forms\Components\TextInput::make('cinema_available')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('sqft')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('wifi_speed')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('max_person')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('price_per_day')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('full_support_available')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('gym_area_available')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('mini_cafe_available')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('cinema_available')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListListings::route('/'),
+            'create' => Pages\CreateListing::route('/create'),
+            'edit' => Pages\EditListing::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
--- RentHouse-Backend/app/Models/Listing.php
+++ RentHouse-Backend/app/Models/Listing.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Support\Str;
 class Listing extends Model
 {
     use HasFactory,SoftDeletes;
@@ -29,4 +29,14 @@ class Listing extends Model
     protected $cast = [
         'attachments' => 'array'
     ];
+
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
