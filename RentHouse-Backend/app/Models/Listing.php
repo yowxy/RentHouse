408d1910dbd76196ad6273439ff7534daec33685
Iklil <iklilblender@gmail.com>
2024-12-01 14:05:07 +0700

    feat: forgot add some fillable

--- RentHouse-Backend/app/Models/Listing.php
+++ RentHouse-Backend/app/Models/Listing.php
@@ -10,5 +10,23 @@ class Listing extends Model
 {
     use HasFactory,SoftDeletes;
 
-    
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'address',
+        'sqft',
+        'wifi_speed',
+        'max_person',
+        'price_per_day',
+        'attachments',
+        'full_support_available',
+        'gym_area_available',
+        'mini_cafe_available',
+        'cinema_available',
+    ];
+
+    protected $cast = [
+        'attachments' => 'array'
+    ];
 }
