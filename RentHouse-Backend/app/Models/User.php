9589e0e50742f7e90fb33d6b2d4880bf8b798d07
Iklil <iklilblender@gmail.com>
2024-12-07 01:16:58 +0700

    feat: add Auth MiddleWare to Dashboard

--- RentHouse-Backend/app/Models/User.php
+++ RentHouse-Backend/app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -22,6 +24,11 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return  $this->role === 'admin';
+    }
 
     /**
      * The attributes that should be hidden for serialization.
